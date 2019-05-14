74786d1d31ef31c80cff31727e19be1c33c5c4fc
Juan Cruz Martinez <bajcmartinez@gmail.com>
2019-05-14 13:52:42 -0400

    feat(ai): Model changes
    - Use NVIDIA model instead

--- data.py
+++ data.py
@@ -4,6 +4,7 @@ import numpy as np
 
 class Data:
     def __init__(self, batch_size=512):
+        # self.data_path = '/opt/carnd_p3/data/'
         self.data_path = './data'
         samples = []
         with open('{0}/driving_log.csv'.format(self.data_path)) as csvfile:
@@ -16,46 +17,41 @@ class Data:
             for line in reader:
                 samples.append(line)
 
-        np.random.shuffle(np.array(samples))
+        samples = np.array(samples)
+        np.random.shuffle(samples)
         limit = int(len(samples) * 0.7)
         self.train = samples[:limit]
         self.valid = samples[limit:]
         self.batch_size = batch_size
+        self.augmenting_by = 6
 
     def train_length(self):
-        return len(self.train) * 3
+        return len(self.train) * self.augmenting_by
 
     def valid_length(self):
-        return len(self.valid) * 3
+        return len(self.valid) * self.augmenting_by
 
     def generator(self, samples):
         num_samples = len(samples)
-        batch_size = self.batch_size // 3
+        batch_size = self.batch_size // self.augmenting_by
 
         while True:
             for offset in range(0, num_samples, batch_size):
                 batch_samples = samples[offset:offset + batch_size]
 
-                gen_x = np.zeros((len(batch_samples)*3, 160, 320, 3))
-                gen_y = np.zeros((len(batch_samples)*3, 1))
+                gen_x = np.zeros((len(batch_samples)*3*2, 160, 320, 3))
+                gen_y = np.zeros((len(batch_samples)*3*2, 1))
                 for i, batch_sample in enumerate(batch_samples):
                     center_angle = float(batch_sample[3])
-                    correction = 0.2
+                    correction_factor = 0.2
+                    for c, correction in enumerate([-correction_factor, 0, correction_factor]):
+                        file_name = batch_sample[0].split('/')[-1]
+                        gen_x[i*self.augmenting_by+c] = cv2.imread('{0}/IMG/{1}'.format(self.data_path, file_name))
+                        gen_y[i*self.augmenting_by+c] = center_angle + correction
 
-                    # Center Camera
-                    file_name = batch_sample[0].split('/')[-1]
-                    gen_x[i] = cv2.imread('{0}/IMG/{1}'.format(self.data_path, file_name))
-                    gen_y[i] = center_angle
+                        gen_x[i*self.augmenting_by+c+3] = np.fliplr(gen_x[i*self.augmenting_by+c])
+                        gen_y[i*self.augmenting_by+c+3] = -(center_angle + correction)
 
-                    # Left Camera
-                    file_name = batch_sample[1].split('/')[-1]
-                    gen_x[i] = cv2.imread('{0}/IMG/{1}'.format(self.data_path, file_name))
-                    gen_y[i] = center_angle + correction
-
-                    # Right Camera
-                    file_name = batch_sample[2].split('/')[-1]
-                    gen_x[i] = cv2.imread('{0}/IMG/{1}'.format(self.data_path, file_name))
-                    gen_y[i] = center_angle - correction
 
                 yield np.array(gen_x), np.array(gen_y)
 
--- model.py
+++ model.py
@@ -1,6 +1,8 @@
-from keras import applications
+# from keras import applications
 from keras import Sequential
-from keras.layers import Cropping2D, Dense, Lambda, Flatten, Dropout
+from keras.layers import Cropping2D, Dense, Lambda, Flatten, BatchNormalization, Conv2D, Dropout
+import matplotlib
+import matplotlib.pyplot as plt
 
 from data import Data
 
@@ -8,21 +10,33 @@ class DriverNet:
     def build(self):
         model = Sequential()
 
-        base_model = applications.VGG19(weights='imagenet', include_top=False)
-        for layer in base_model.layers:
-            layer.trainable = False
+        # base_model = applications.VGG19(weights='imagenet', include_top=False)
+        # for layer in base_model.layers:
+        #     layer.trainable = False
 
         model.add(Lambda(lambda x: x / 255 - 0.5, input_shape=(160, 320, 3)))
         model.add(Cropping2D(cropping=((50, 20), (0, 0))))
-        model.add(base_model)
-        model.add(Flatten())
-        model.add(Dense(1024, activation='relu'))
-        model.add(Dropout(rate=0.5))
-        model.add(Dense(512, activation='relu'))
-        model.add(Dropout(rate=0.5))
-        model.add(Dense(1))
+        # model.add(base_model)
 
-        model.compile(loss='mse', optimizer='adam')
+        # nvidia model
+        model.add(BatchNormalization())
+        model.add(Conv2D(24, (5, 5), activation="relu", strides=(2, 2)))
+        model.add(Conv2D(36, (5, 5), activation="relu", strides=(2, 2)))
+        model.add(Conv2D(48, (5, 5), activation="relu", strides=(2, 2)))
+
+        model.add(Conv2D(64, (3, 3), activation="elu", strides=(1, 1)))
+        model.add(Conv2D(64, (3, 3), activation="elu", strides=(1, 1)))
+
+        model.add(Dropout(0.5))
+        model.add(Flatten())
+
+        model.add(Dense(1164, activation='relu'))
+        model.add(Dense(100, activation='relu'))
+        model.add(Dense(50, activation='relu'))
+        model.add(Dense(10, activation='relu'))
+        model.add(Dense(1, activation='relu'))
+
+        model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
         self.model = model
 
@@ -31,17 +45,31 @@ class DriverNet:
         train_generator = data.train_generator()
         valid_generator = data.valid_generator()
 
-        self.model.fit_generator(train_generator,
+        history = self.model.fit_generator(train_generator,
                                  steps_per_epoch=data.train_length() / batch_size,
                                  validation_data=valid_generator,
                                  validation_steps=data.valid_length() / batch_size,
                                  epochs=epochs)
 
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(history.history['acc'])
+        ax.plot(history.history['val_acc'])
+        ax.set_title('Model Accuracy')
+        ax.set_ylabel('Accuracy')
+        ax.set_xlabel('Epoch')
+        ax.legend(['train', 'test'], loc='upper left')
+        fig.savefig('train_evol.png')
+
+        print("Saving model...")
         self.model.save("./model.h5")
 
 if __name__ == '__main__':
     net = DriverNet()
+    print("Building network...")
     net.build()
+    print("Network Summary...")
     net.model.summary()
-    net.train()
+    print()
+    net.train(batch_size=100, epochs=5)
 
